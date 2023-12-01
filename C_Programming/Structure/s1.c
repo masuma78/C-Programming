1f326e63886f85a6db420d681d3ce10e30199ed1
Masuma Akter Akhi <130857056+masuma78@users.noreply.github.com>
2023-12-01 09:54:12 +0600

    Add files via upload

---
+++ C_Programming/Structure/s1.c
@@ -0,0 +1,28 @@
+/*Write a Code in C to Store information about a student using structure.*/
+#include<stdio.h>
+struct student{
+    char name[20];
+    int id;
+    float marks;
+}s;
+
+ int main() {
+    
+    printf("Enter the name: ");
+    scanf("%s",s.name);
+    printf("Enter the id: ");
+    scanf("%d",&s.id);
+    printf("Enter the marks: ");
+    scanf("%f",&s.marks);
+
+    printf("\nThe student information\n");
+    printf("---------------------------\n");
+
+    printf("\nName: %s\n",s.name);
+    printf("\nID: %d\n",s.id);
+    printf("\nMarks: %.2f\n",s.marks);
+    printf("\n");
+
+
+    return 0;
+ }
